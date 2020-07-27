7222047fce7e3cce112f1876c22e19e23dc61baa
Jack Devey <38474124+jwdevey@users.noreply.github.com>
2020-07-27 14:05:07 +0100

    v1.0

--- app/src/main/java/org/bandev/buddhaquotes/MainActivity.java
+++ app/src/main/java/org/bandev/buddhaquotes/MainActivity.java
@@ -3,6 +3,14 @@ package org.bandev.buddhaquotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.widget.Toolbar;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        WebView browser = (WebView) findViewById(R.id.webview);
+        browser.loadUrl("file:///android_asset/index.html");
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
     }
 }
