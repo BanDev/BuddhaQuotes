1a5e30a6f9fd592535c35390688ce74493e1798e
Jack Devey <38474124+jwdevey@users.noreply.github.com>
2020-09-24 16:45:58 +0100

    fix

--- app/src/main/java/org/bandev/buddhaquotes/License.kt
+++ app/src/main/java/org/bandev/buddhaquotes/License.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowInsets
@@ -18,7 +19,6 @@ import java.io.*
 
 class License : AppCompatActivity() {
 
-    private var stream: InputStream? = null
 
     //Say that you need a snazzy phone to use the app (Android Ver > R)
     @RequiresApi(Build.VERSION_CODES.R)
@@ -27,27 +27,25 @@ class License : AppCompatActivity() {
         setContentView(R.layout.activity_license)
 
         //Setup toolbar which is the top bar thingy
-        val mToolBar = findViewById<View>(R.id.toolbar) as Toolbar
-        setSupportActionBar(mToolBar)
+        val myToolbar = findViewById<View>(R.id.my_toolbar) as Toolbar
+        setSupportActionBar(myToolbar)
         (supportActionBar ?: return).setDisplayShowTitleEnabled(false)
+
+
         (supportActionBar ?: return).setDisplayHomeAsUpEnabled(true)
 
-        //Enable edge to edge in newer android versions (Hides top and bottom bars)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        //Some magic to find the height of the devices status bar
         var statusBarHeight = 0
         val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
         if (resourceId > 0) {
             statusBarHeight = resources.getDimensionPixelSize(resourceId)
         }
 
-        //Now use the statusBarHeight to make sure you can see the toolbar where it should be
-        val param = mToolBar.layoutParams as ViewGroup.MarginLayoutParams
+        val param = myToolbar.layoutParams as ViewGroup.MarginLayoutParams
         param.setMargins(0, statusBarHeight, 0, 0)
-        mToolBar.layoutParams = param
+        myToolbar.layoutParams = param
 
-        //Now tell android to show window insets
         val view = View(this)
         view.doOnLayout {
             view.windowInsetsController?.show(WindowInsets.Type.ime())
@@ -55,35 +53,30 @@ class License : AppCompatActivity() {
             window.insetsController?.show(WindowInsets.Type.ime())
         }
 
-        //Ngl have no clue what this does (Oh ye its to pad the bottom so it doesn't appear under the nav bar)
         view.setOnApplyWindowInsetsListener { view, insets ->
             view.updatePadding(bottom = insets.systemWindowInsetBottom)
             insets
         }
-
-        //Identify the TextView and then start loading the licenses into it
-        val textview: TextView = findViewById(R.id.license)
-        stream = File("Licenses/app.txt").inputStream()
-        val bytes = ByteArray(32)
-        loadFileIntoTextView((stream as FileInputStream).read(bytes), textview)
     }
 
-    private fun loadFileIntoTextView(@RawRes fileId: Int, textView: TextView) {
-        val sb = StringBuilder()
-        val br = BufferedReader(InputStreamReader(resources.openRawResource(fileId)))
-
-        try {
-            var line: String? = br.readLine()
-            while (line != null) {
-                sb.append(line)
-                sb.append('\n')
-                line = br.readLine()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
             }
-        } catch (e: IOException) {
-            Log.w("LicenseActivity", e)
+            else -> super.onOptionsItemSelected(item)
         }
+    }
 
-        stream?.close()
-        textView.text = sb.toString()
+    override fun onBackPressed() {
+        super.onBackPressed()
+        // add your animation
+        finish()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
