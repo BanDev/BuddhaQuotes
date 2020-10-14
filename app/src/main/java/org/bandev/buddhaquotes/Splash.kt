03f252c288196145488a28b14a6d8113b216dafa
Russell Banks <Rusco_exe@protonmail.com>
2020-10-15 00:19:59 +0100

    Minor fixes and optimisations

--- app/src/main/java/org/bandev/buddhaquotes/Splash.kt
+++ app/src/main/java/org/bandev/buddhaquotes/Splash.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.WindowCompat
 
 class Splash : AppCompatActivity() {
 
