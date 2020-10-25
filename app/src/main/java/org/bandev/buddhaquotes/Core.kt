810890d0d591abc4409af92f036d99097cfc2a1b
Jack <jack.exe@protonmail.com>
2020-10-25 15:30:04 +0000

    Update Core.kt

--- app/src/main/java/org/bandev/buddhaquotes/Core.kt
+++ app/src/main/java/org/bandev/buddhaquotes/Core.kt
@@ -26,7 +26,7 @@ class Core(val context: Context) {
          * @author jack.txt
          */
 
-        fun newItem(list: String, num: Int) {
+        fun newItemInt(list: String, num: Int) {
             val pref = context.getSharedPreferences("List_system", 0)
             val editor = pref.edit()
             val list_arr = pref.getString(list, "")
@@ -39,11 +39,30 @@ class Core(val context: Context) {
 
 
         /**
+         * Updates a 'list' (adds a quote to a pre-existing 'list')
+         * @param [list] title of list (String)
+         * @param [quote] text of quote (String)
+         * @author jack.txt
+         */
+
+        fun newItemString(list: String, quote: String) {
+            val pref = context.getSharedPreferences("List_system", 0)
+            val editor = pref.edit()
+            val list_arr = pref.getString(list, "")
+            val list_arr_final = LinkedList(list_arr?.split("//"))
+            list_arr_final.push(quote)
+            val string_out = list_arr_final.joinToString(separator = "//")
+            editor.putString(list, string_out)
+            editor.commit()
+        }
+
+        /**
          * Updates a 'list' (removes a quote from a pre-existing 'list')
          * @param [list] title of list (String)
          * @param [num] id of quote (Int)
          * @author jack.txt
          */
+
 
         fun removeItem(list: String, num: Int) {
             val pref = context.getSharedPreferences("List_system", 0)
@@ -56,12 +75,12 @@ class Core(val context: Context) {
             editor.commit()
         }
 
-
         /**
          * Creates a 'List' (adds title to master list, generates a sub-list under title)
          * @param [text] title of new list (String)
          * @author jack.txt
          */
+
 
         fun newList(text: String) {
             val pref = context.getSharedPreferences("List_system", 0)
@@ -75,12 +94,12 @@ class Core(val context: Context) {
             editor.commit()
         }
 
-
         /**
          * Deletes a 'List' (removes title from master list, clears sub-list under title)
          * @param [text] title of list to be deleted (String)
          * @author jack.txt
          */
+
 
         fun removeList(text: String) {
             val pref = context.getSharedPreferences("List_system", 0)
@@ -94,7 +113,6 @@ class Core(val context: Context) {
             editor.commit()
         }
 
-
         /**
          * Takes a 'list' and writes outputs it as string with seperators
          * @param [list] title of list (String)
@@ -102,6 +120,7 @@ class Core(val context: Context) {
          * @return list as text in readable format (String)
          * @author jack.txt
          */
+
 
         fun writeList(list: String, seperator: String): String {
             val pref = context.getSharedPreferences("List_system", 0)
