3c5fa4f59ae5e311fe7eab393c587ac997d6ddef
Jack <jack.exe@protonmail.com>
2020-10-25 17:36:32 +0000

    Widget improvements

--- app/src/main/java/org/bandev/buddhaquotes/Widget.kt
+++ app/src/main/java/org/bandev/buddhaquotes/Widget.kt
@@ -1,23 +1,65 @@
 package org.bandev.buddhaquotes
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
+
 
 /**
  * Implementation of App Widget functionality.
  * App Widget Configuration implemented in [WidgetConfigureActivity]
  */
 class Widget : AppWidgetProvider() {
+
+    var WIDGET_BUTTON = "org.bandev.buddhaquotes.WIDGET_BUTTON"
+    var WIDGET_LIKE = "org.bandev.buddhaquotes.WIDGET_LIKE"
+    var QUOTE_CURRENT = "Test"
+    private val views: RemoteViews? = null
+    private var x = 0
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
+
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if (intent.action.equals(WIDGET_BUTTON, ignoreCase = true)) {
+            var views = RemoteViews(context.packageName, R.layout.widget)
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val appWidgetIds = appWidgetManager.getAppWidgetIds(
+                ComponentName(
+                    context,
+                    Widget::class.java
+                )
+            )
+            x++
+            QUOTE_CURRENT = Quotes().random(0)
+            views.setTextViewText(R.id.appwidget_text, QUOTE_CURRENT)
+            appWidgetManager.updateAppWidget(appWidgetIds, views)
+        }else if (intent.action.equals(WIDGET_LIKE, ignoreCase = true)) {
+            var views = RemoteViews(context.packageName, R.layout.widget)
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val appWidgetIds = appWidgetManager.getAppWidgetIds(
+                ComponentName(
+                    context,
+                    Widget::class.java
+                )
+            )
+            x++
+            Core(context).Lists().newItemString("Favourites", QUOTE_CURRENT)
+            views.setImageViewResource(R.id.like, R.drawable.heart_full_red)
+            appWidgetManager.updateAppWidget(appWidgetIds, views)
         }
     }
 
@@ -35,6 +77,8 @@ class Widget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+
 }
 
 internal fun updateAppWidget(
@@ -42,11 +86,28 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = loadTitlePref(context, appWidgetId)
-    // Construct the RemoteViews object
+// Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setImageViewResource(R.id.refresh, R.drawable.ic_refresh_black)
+    //views.setImageViewResource(R.id.like, R.drawable.like)
+    views.setOnClickPendingIntent(R.id.refresh, getPenIntent(context))
+    //views.setOnClickPendingIntent(R.id.like, getPenIntent2(context))
 
+     views.setTextViewText(R.id.appwidget_text, Quotes().random(0))
+
+    getPenIntent(context)
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
+}
+
+fun getPenIntent(context: Context): PendingIntent {
+    val intent = Intent(context, Widget::class.java)
+    intent.action = Widget().WIDGET_BUTTON
+    return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+}
+
+fun getPenIntent2(context: Context): PendingIntent {
+    val intent = Intent(context, Widget::class.java)
+    intent.action = Widget().WIDGET_LIKE
+    return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 }
