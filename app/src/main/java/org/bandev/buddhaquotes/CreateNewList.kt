824d5aae7d2fdff647c390d7fceb0e991c46db71
Jack <jack.exe@protonmail.com>
2020-11-08 18:31:51 +0000

    Imroved create new list & swipe to dismiss

--- app/src/main/java/org/bandev/buddhaquotes/CreateNewList.kt
+++ app/src/main/java/org/bandev/buddhaquotes/CreateNewList.kt
@@ -5,10 +5,16 @@ import android.content.pm.ActivityInfo
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import com.google.android.material.textfield.TextInputLayout
 import org.bandev.buddhaquotes.core.Colours
 import org.bandev.buddhaquotes.core.Compatibility
 import org.bandev.buddhaquotes.core.Languages
@@ -41,23 +47,50 @@ class CreateNewList : AppCompatActivity() {
         //Setup button & onclick
         go = findViewById(R.id.go)
         go.setOnClickListener {
+            val name_box = findViewById<TextInputLayout>(R.id.name_box)
             name = findViewById(R.id.name)
             text = name.text.toString()
 
-            //Add new list to MASTER_LIST & create a list for itself
-            val pref = getSharedPreferences("List_system", 0)
-            val editor = pref.edit()
-            editor.putString(text, "null")
-            editor.putString(
-                "MASTER_LIST",
-                (pref.getString("MASTER_LIST", "Favourites") + "//" + text)
-            )
-            editor.apply()
 
-            //Leave & take users back to favourites
-            val myIntent = Intent(this, Favourites::class.java)
-            this.startActivity(myIntent)
-            finish()
+
+            val error = getError(text)
+
+            if(error != "safe"){
+                name_box.error = error
+            }else{
+                //Add new list to MASTER_LIST & create a list for itself
+                val pref = getSharedPreferences("List_system", 0)
+                val editor = pref.edit()
+                editor.putString(text, "null")
+                editor.putString(
+                    "MASTER_LIST",
+                    (pref.getString("MASTER_LIST", "Favourites") + "//" + text)
+                )
+                editor.apply()
+
+                //Leave & take users back to favourites
+                val myIntent = Intent(this, Favourites::class.java)
+                this.startActivity(myIntent)
+                finish()
+            }
+
+
+        }
+    }
+
+    fun getError(input: String ): String{
+        val pref = getSharedPreferences("List_system", 0)
+
+        if(input == ""){
+            return "Cannot be blank"
+        }else if(input.contains("//")){
+            return "Cannot contain //"
+        }else if(input == "Favourites"){
+            return ":( stop tryna break our app"
+        }else if(pref.getString("MASTER_LIST", "Favourites")!!.contains(input)){
+            return "There is already a list named " + input
+        }else{
+            return "safe"
         }
     }
 
