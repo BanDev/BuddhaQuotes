439f722c35b8535ee2cb053e1a495283eb260beb
Jack <jack@bandev.uk>
2020-12-16 19:41:29 +0000

    Added pom.xml

--- gradlew.bat
+++ gradlew.bat
@@ -1,70 +1,84 @@
-package org.bandev.buddhaquotes
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
 
-import android.graphics.drawable.Drawable
-import android.os.Build
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GestureDetectorCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
 
-class MainActivity : AppCompatActivity() {
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
 
-    val done: Boolean? = null
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
 
-    val textview: TextView? = null
-    val quote = Quote()
-    val fab: FloatingActionButton? = null
-    val fab2: FloatingActionButton? = null
-    val list: List<String?>? = null
-    var favs: Array<String?>
-    var array: Array<String?>
-    val font_size: String? = null
-    val DEBUG_TAG = "Gestures"
-    var toolbar: Toolbar? = null
-    val mDetector: GestureDetectorCompat? = null
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        //Setup Activity
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
 
-        //Toolbar
-        toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar!!.setDisplayShowTitleEnabled(false)
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
 
+goto fail
 
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.heart_black);
-        myToolbar.setNavigationIcon(drawable);
-        setSupportActionBar(myToolbar);
+if exist "%JAVA_EXE%" goto init
 
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
 
-        textview = findViewById(R.id.text);
-        fab = findViewById(R.id.refresh);
-        fab2 = findViewById(R.id.fav2);
+goto fail
 
+:init
+@rem Get command-line arguments, handling Windows variants
 
+if not "%OS%" == "Windows_NT" goto win9xME_args
 
-        SharedPreferences sharedPreferences = getSharedPreferences("Settings", 0);
-        String text_size = sharedPreferences.getString("text_size", "md");
-        Log.d("Size", text_size);
-        switch(text_size){
-            case "sm":
-                font_size = "30";
-                break;
-            case "lg":
-                font_size = "50";
-                break;
-            default:
-                font_size = "40";
-                break;
-        }
-        Log.d("text_size", font_size);
+:win9xME_args
+@rem Slurp the command line arguments.
+set CMD_LINE_ARGS=
+set _SKIP=2
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_
+:win9xME_args_slurp
+if "x%~1" == "x" goto execute
+
+set CMD_LINE_ARGS=%*
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+
+:end
+@rem End local scope for the variables with windows NT shell
+if "%ERRORLEVEL%"=="0" goto mainEnd
+
+:fail
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ return code!
+if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+exit /b 1
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
+
+:omega
