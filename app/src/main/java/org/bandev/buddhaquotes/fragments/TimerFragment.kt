491c842692d45299569e9c56663c9514f7714168
Jack <jack@bandev.uk>
2020-12-29 14:39:10 +0000

    Added countdown timer

--- app/src/main/java/org/bandev/buddhaquotes/fragments/TimerFragment.kt
+++ app/src/main/java/org/bandev/buddhaquotes/fragments/TimerFragment.kt
@@ -1,6 +1,7 @@
 package org.bandev.buddhaquotes.fragments
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,6 +24,13 @@ class TimerFragment : Fragment() {
 
     private var _binding: FragmentTimerBinding? = null
     private val binding get() = _binding!!
+    lateinit var countdown_timer: CountDownTimer
+    lateinit var timeSheet: TimeSheet
+    var isRunning: Boolean = false
+    var isPaused: Boolean = false
+    var time_in_milli_seconds = 0L
+    var duration = 0L
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,12 +42,62 @@ class TimerFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        TimeSheet().show(requireContext()) {
+        timeSheet = TimeSheet().build(requireContext()) {
             title("Meditation timer")
             format(TimeFormat.MM_SS)
             onPositive { durationTimeInMillis: Long ->
-                // Handle selected time
+                duration = durationTimeInMillis * 1000
+                startTimer(duration)
             }
         }
+        binding.button.setOnClickListener {
+            if (isRunning) {
+                pauseTimer()
+            } else if (isPaused) {
+                startTimer(time_in_milli_seconds)
+            } else
+                timeSheet.show()
+        }
+        binding.stop.setOnClickListener {
+            countdown_timer.cancel()
+            updateTextUI(0)
+            binding.button.text = "Set Timer"
+            isRunning = false
+            isPaused = false
+            binding.stop.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun startTimer(time_in_seconds: Long) {
+        countdown_timer = object : CountDownTimer(time_in_seconds, 1000) {
+            override fun onFinish() {
+
+            }
+
+            override fun onTick(p0: Long) {
+                time_in_milli_seconds = p0
+                updateTextUI(time_in_milli_seconds)
+            }
+        }
+        countdown_timer.start()
+
+        isRunning = true
+        binding.button.text = "Pause"
+        binding.stop.visibility = View.VISIBLE
+    }
+
+    internal fun updateTextUI(time: Long) {
+        val minute = (time / 1000) / 60
+        val seconds = (time / 1000) % 60
+
+        binding.timeLeft.text = "$minute:$seconds"
+    }
+
+    private fun pauseTimer() {
+        binding.button.text = "Start"
+        isPaused = true
+        countdown_timer.cancel()
+        isRunning = false
+        binding.stop.visibility = View.VISIBLE
     }
 }
