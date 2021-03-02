bcbf856adac27edf8fcc029f2740204b2ef49f94
Jack Devey <jack@bandev.uk>
2021-03-02 22:13:58 +0000

    More bug fixes

--- app/src/main/java/org/bandev/buddhaquotes/activities/Migrate.kt
+++ app/src/main/java/org/bandev/buddhaquotes/activities/Migrate.kt
@@ -2,9 +2,7 @@ package org.bandev.buddhaquotes.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import org.bandev.buddhaquotes.R
 import org.bandev.buddhaquotes.core.Lists
 import org.bandev.buddhaquotes.core.ListsV2
 import org.bandev.buddhaquotes.core.Quotes
@@ -23,6 +21,14 @@ class Migrate : AppCompatActivity() {
         binding.yes.setOnClickListener { begin() }
 
         val sharedPrefs = getSharedPreferences("Settings", 0)
+        val editor = sharedPrefs.edit()
+
+        if (sharedPrefs.getBoolean("first_time", true)) {
+            editor.putBoolean("first_time", false)
+            editor.apply()
+            startActivity(Intent(this, Intro::class.java))
+        }
+
         if (!sharedPrefs.getBoolean("old_quotes", true)){
             startActivity(Intent(this, Main::class.java))
         }
@@ -33,16 +39,13 @@ class Migrate : AppCompatActivity() {
         val newLists = ListsV2(this)
         val quotes = Quotes()
         for (listTitle in lists.getMasterList(this)) {
-            Toast.makeText(this, "Started translating $listTitle", Toast.LENGTH_SHORT).show()
             val list = lists.getList(listTitle, this)
             val newList = mutableListOf<Int>()
             for (quote in list) {
                 if (quote == "") continue
                 val id = quotes.getFromString(quote, this)
-                Toast.makeText(this, "$quote -> $id", Toast.LENGTH_SHORT).show()
                 newList.add(id)
             }
-            Toast.makeText(this, "translating of $listTitle complete", Toast.LENGTH_SHORT).show()
             newList.add(-1)
             newLists.newList(listTitle, newList)
         }
