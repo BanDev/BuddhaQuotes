bb2c13a7aaeeed96d9c47e7da0be30ec5d290fcc
Jack Devey <jack@bandev.uk>
2021-03-02 22:11:03 +0000

    Remove buggy code

--- app/src/main/java/org/bandev/buddhaquotes/activities/Migrate.kt
+++ app/src/main/java/org/bandev/buddhaquotes/activities/Migrate.kt
@@ -32,13 +32,7 @@ class Migrate : AppCompatActivity() {
         val lists = Lists()
         val newLists = ListsV2(this)
         val quotes = Quotes()
-        var first = true
         for (listTitle in lists.getMasterList(this)) {
-            if (first){
-                first = false
-                continue
-            }
-            first = false
             Toast.makeText(this, "Started translating $listTitle", Toast.LENGTH_SHORT).show()
             val list = lists.getList(listTitle, this)
             val newList = mutableListOf<Int>()
