72212b43a1a002e7295157ead38b4ef433d6ba34
Russell Banks <Rusco_exe@protonmail.com>
2021-08-17 20:27:01 +0100

    Add three more images to quote fragment
    + Alphabetise options in the sheet to change the image on the quote fragment

--- app/src/main/java/org/bandev/buddhaquotes/core/Images.kt
+++ app/src/main/java/org/bandev/buddhaquotes/core/Images.kt
@@ -28,16 +28,20 @@ object Images {
         return if (liked) R.drawable.ic_heart_red else R.drawable.ic_heart_outline
     }
 
-    const val MONK: Int = 1
-    const val DHARMA_WHEEL: Int = 2
-    const val ANAHATA: Int = 3
-    const val MANDALA: Int = 4
-    const val ENDLESS_KNOT: Int = 5
-    const val ELEPHANT: Int = 6
-    const val TEMPLE: Int = 7
-    const val LAMP: Int = 8
-    const val SHRINE: Int = 9
-    const val LOTUS: Int = 10
-    const val LOTUS_WATER: Int = 11
+    const val ANAHATA: Int = 0
+    const val BELL: Int = 1
+    const val ELEPHANT: Int = 3
+    const val ENDLESS_KNOT: Int = 4
+    const val LAMP: Int = 5
+    const val LOTUS: Int = 6
+    const val MANDALA: Int = 7
+    const val MONK: Int = 8
+    const val RATTLE: Int = 9
+    const val SHRINE: Int = 10
+    const val STUPA: Int = 11
+    const val TEMPLE: Int = 12
+    const val LOTUS_WATER: Int = 13
+    const val DHARMA_WHEEL: Int = 14
+    const val NO_IMAGE: Int = 15
 
 }
