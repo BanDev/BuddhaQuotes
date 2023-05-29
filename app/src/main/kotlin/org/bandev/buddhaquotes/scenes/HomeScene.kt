4b2d3ffbfbbc2b8df4113171475acbd6424c256f
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2023-05-29 23:18:20 +0100

    Add sheet to change home screen image

--- app/src/main/kotlin/org/bandev/buddhaquotes/scenes/HomeScene.kt
+++ app/src/main/kotlin/org/bandev/buddhaquotes/scenes/HomeScene.kt
@@ -29,17 +29,23 @@ import androidx.compose.material.icons.rounded.ChevronLeft
 import androidx.compose.material.icons.rounded.ChevronRight
 import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
@@ -50,6 +56,13 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.maxkeppeker.sheets.core.models.base.IconSource
+import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
+import com.maxkeppeler.sheets.option.OptionView
+import com.maxkeppeler.sheets.option.models.DisplayMode
+import com.maxkeppeler.sheets.option.models.Option
+import com.maxkeppeler.sheets.option.models.OptionConfig
+import com.maxkeppeler.sheets.option.models.OptionSelection
 import kotlin.random.Random
 import kotlinx.coroutines.launch
 import org.bandev.buddhaquotes.FavoriteButton
@@ -60,7 +73,7 @@ import org.bandev.buddhaquotes.items.AnimatedHeart
 import org.bandev.buddhaquotes.items.Heart
 import org.bandev.buddhaquotes.items.QuoteItem
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScene(navController: NavController, pagerState: PagerState, viewModel: BuddhaQuotesViewModel = viewModel()) {
     val quote by viewModel.selectedQuote.collectAsStateWithLifecycle(QuoteItem())
@@ -68,6 +81,9 @@ fun HomeScene(navController: NavController, pagerState: PagerState, viewModel: B
     val scope = rememberCoroutineScope()
 
     val hearts = remember { mutableStateListOf<Heart>() }
+
+    var imageSheetVisible by rememberSaveable { mutableStateOf(false) }
+    val imageSheetState = rememberModalBottomSheetState()
 
     Scaffold(
         bottomBar = {
@@ -91,6 +107,7 @@ fun HomeScene(navController: NavController, pagerState: PagerState, viewModel: B
             }
         }
     ) { paddingValues ->
+        var centerImage by remember { mutableStateOf(R.drawable.image_anahata) }
         HorizontalPager(
             pageCount = 3,
             beyondBoundsPageCount = 2,
@@ -157,9 +174,17 @@ fun HomeScene(navController: NavController, pagerState: PagerState, viewModel: B
                             verticalArrangement = Arrangement.Bottom
                         ) {
                             Image(
-                                painter = painterResource(id = R.drawable.image_anahata),
+                                painter = painterResource(id = centerImage),
                                 contentDescription = null,
-                                modifier = Modifier.size(250.dp)
+                                modifier = Modifier
+                                    .size(250.dp)
+                                    .pointerInput(Unit) {
+                                        detectTapGestures(
+                                            onLongPress = {
+                                                imageSheetVisible = true
+                                            }
+                                        )
+                                    }
                             )
                             ElevatedCard(Modifier.padding(20.dp)) {
                                 Row(
@@ -234,6 +259,86 @@ fun HomeScene(navController: NavController, pagerState: PagerState, viewModel: B
                 }
                 1 -> ListsScene(navController = navController)
                 else -> MeditateScene()
+            }
+        }
+        if (imageSheetVisible) {
+            ModalBottomSheet(
+                onDismissRequest = { imageSheetVisible = false },
+                sheetState = imageSheetState
+            ) {
+                OptionView(
+                    useCaseState = rememberUseCaseState(),
+                    selection = OptionSelection.Single(
+                        options = listOf(
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_anahata),
+                                titleText = "Anahata",
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_bell),
+                                titleText = "Bell"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_dharma_wheel),
+                                titleText = "Dharma Wheel"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_elephant),
+                                titleText = "Elephant"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_endless_knot),
+                                titleText = "Endless Knot"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_lamp),
+                                titleText = "Lamp"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_lotus),
+                                titleText = "Lotus"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_lotus_water),
+                                titleText = "Water Lotus"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_mandala),
+                                titleText = "Mandala"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_monk),
+                                titleText = "Monk"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_rattle),
+                                titleText = "Rattle"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_shrine),
+                                titleText = "Shrine"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_stupa),
+                                titleText = "Stupa"
+                            ),
+                            Option(
+                                icon = IconSource(drawableRes = R.drawable.image_temple),
+                                titleText = "Temple"
+                            )
+                        ),
+                        withButtonView = false,
+                        onSelectOption = { _, option ->
+                            scope.launch { imageSheetState.hide() }.invokeOnCompletion {
+                                if (!imageSheetState.isVisible) {
+                                    imageSheetVisible = false
+                                }
+                            }
+                            option.icon?.drawableRes?.let { centerImage = it }
+                        }
+                    ),
+                    config = OptionConfig(mode = DisplayMode.GRID_VERTICAL)
+                )
             }
         }
     }
