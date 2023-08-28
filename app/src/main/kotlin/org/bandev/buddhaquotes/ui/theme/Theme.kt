c7ca208b6befe4b733cf2de2516f7f584e07119e
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2023-08-28 23:20:07 +0100

    Remove deprecated Accompanist SystemUIController

--- app/src/main/kotlin/org/bandev/buddhaquotes/ui/theme/Theme.kt
+++ app/src/main/kotlin/org/bandev/buddhaquotes/ui/theme/Theme.kt
@@ -6,10 +6,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 internal const val SpringDefaultDampingRatio = Spring.DampingRatioMediumBouncy
 internal const val SpringDefaultStiffness = Spring.StiffnessLow
@@ -26,25 +23,8 @@ fun BuddhaQuotesTheme(
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        content = content
-    )
-}
-
-@Composable
-fun EdgeToEdgeContent(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val systemUiController = rememberSystemUiController()
-
-    DisposableEffect(systemUiController, darkTheme) {
-        systemUiController.setSystemBarsColor(
-            color = Color.Transparent,
-            darkIcons = !darkTheme
-        )
-
-        onDispose {}
+        colorScheme = colorScheme
+    ) {
+        content()
     }
-    content()
 }
